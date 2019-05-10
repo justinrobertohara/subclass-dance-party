85f6cc9b1244c6306a6f369525394a3b3aa23f83
Bryan Nguyen <bryan.m.nguyen12@gmail.com>
2019-05-10 15:37:09 -0700

    Complete adding css

--- src/rerunDancer.js
+++ src/rerunDancer.js
@@ -3,6 +3,8 @@ var MakeRerunDancer = function(top, left, timeBetweenSteps) {
   this.$node.append('<img src="http://i11.photobucket.com/albums/a156/0access/Rerun.gif" height="80px" width="80px">');
   this.$node.css("border", "hidden");
 
+  this.$node.addClass('rotate')
+
 };
 
 MakeRerunDancer.prototype = Object.create(MakeDancer.prototype);
